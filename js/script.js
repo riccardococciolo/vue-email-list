5737145acecc0bc6dcfd2425ffbc5e57b2f84b17
riccardococciolo <73590570+riccardococciolo@users.noreply.github.com>
2023-11-21 16:11:51 +0100

    add: email creator

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            emails: [],
+        };
+    },
+    created() {
+        for (let i = 0; i < 10; i++) {
+            axios
+            .get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((resp) => {
+                console.log(resp);
+                this.emails.push(resp.data.response);
+            }); 
+        }
+    },
+}).mount("#app");
